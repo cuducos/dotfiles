8ed221ee7e70a78c2c815621405bd155f1d57edd
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-09-02 09:47:54 -0400

    Fixes ruby.nvim config file

--- .config/nvim/lua/config/ruby.lua
+++ .config/nvim/lua/config/ruby.lua
@@ -1,3 +1,4 @@
+require("ruby_nvim").setup({test_cmd = "rails test"})
 vim.api.nvim_set_keymap(
-  "n", "<Leader>tt", "<Cmd>SymbolsOutline<CR>", {noremap = true}
+  "n", "<Leader>al", "<Cmd>RubyAlternate<CR>", {noremap = true}
 )
