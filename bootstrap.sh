c22787989126ca58e2f624281e990c4e45981755
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-09-16 17:09:03 -0400

    Adds Neovim's Packer to the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,3 @@
 python -c "import sys; assert sys.version_info.major == 3 and sys.version_info.minor >= 6" && python symlinks.py
+git clone https://github.com/wbthomason/packer.nvim "$HOME/.local/share/neovim/site/pack/packer/start/packer.nvim"
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'packadd packer.nvim | PackerSync'
