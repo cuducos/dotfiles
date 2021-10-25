c3f35a4a7fd61c28195cc6e9e6a9835bfdc579fc
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-10-25 17:37:53 -0400

    Cleans up nvim-cmp

--- .config/nvim/lua/config/cmp.lua
+++ .config/nvim/lua/config/cmp.lua
@@ -1,51 +1,28 @@
 local cmp = require("cmp")
 
-local check_back_space = function()
-  local col = vim.fn.col(".") - 1
-  if col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-    return true
-  else
-    return false
-  end
-end
-
 cmp.setup(
   {
     mapping = {
+      ["<Tab>"] = cmp.mapping.select_next_item(),
       ["<S-Tab>"] = cmp.mapping.select_prev_item(),
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.close(),
       ["<CR>"] = cmp.mapping.confirm(
         {behavior = cmp.ConfirmBehavior.Insert, select = true}
       ),
-      ["<Tab>"] = function(fallback)
-        if vim.fn.pumvisible() == 1 then
-          vim.fn.feedkeys(
-            vim.api.nvim_replace_termcodes("<C-n>", true, true, true), "n"
-          )
-        elseif check_back_space() then
-          vim.fn.feedkeys(
-            vim.api.nvim_replace_termcodes("<Tab>", true, true, true), "n"
-          )
-        else
-          fallback()
-        end
-      end,
     },
     sources = {
-      {name = "buffer"},
-      {name = "calc"},
-      {name = "emoji"},
-      {name = "look"},
-      {name = "luasnip"},
-      {name = "nvim_lsp"},
       {name = "nvim_lua"},
-      {name = "path"},
-      {name = "spell"},
+      {name = "nvim_lsp"},
       {name = "tags"},
       {name = "treesitter"},
-      {name = "ultisnips"},
+      {name = "path"},
       {name = "vsnip"},
+      {name = "buffer", keyword_length = 5},
+      {name = "spell", keyword_length = 5},
+      {name = "emoji"},
+      {name = "calc"},
     },
+    experimental = {ghost_text = true},
   }
 )
