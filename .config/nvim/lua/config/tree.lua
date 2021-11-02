e6ff328c82e4df76f3806900e2504c718585f0e2
Eduardo Cuducos <cuducos@users.noreply.github.com>
2021-11-02 10:42:56 -0400

    Updates nvim-tree config to new format

--- .config/nvim/lua/config/tree.lua
+++ .config/nvim/lua/config/tree.lua
@@ -1,18 +1,19 @@
 vim.g.nvim_tree_add_trailing = 1
 vim.g.nvim_tree_highlight_opened_files = 1
-vim.g.nvim_tree_ignore = {
-  [[\.pyc$]],
-  "__pycache__",
-  ".git",
-  ".DS_Store",
-  ".ropeproject",
-  ".coverage",
-  "cover/",
-}
 vim.g.nvim_tree_width = 36
 vim.g.nvim_tree_width_allow_resize = 1
 
-require('nvim-tree').setup({})
+require("nvim-tree").setup {
+  ignore = {
+    [[\.pyc$]],
+    "__pycache__",
+    ".git",
+    ".DS_Store",
+    ".ropeproject",
+    ".coverage",
+    "cover/",
+  },
+}
 
 vim.api.nvim_set_keymap(
   "n", "<Leader>nt", "<Cmd>NvimTreeToggle<CR>", {noremap = true}
