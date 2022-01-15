e274fe42f98a800d14d3e6d30b8a72363f051d32
Eduardo Cuducos <cuducos@users.noreply.github.com>
2022-01-15 11:24:16 -0500

    Replaces: symlinks.py -> bootstrap.sh

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ A work in progress that started [with @ellisonleao](https://github.com/npxbr/fro
 ## Replacing files in `~/.config` by symlinks with the files in this repo
 
 ```console
-$ python symlinks.py  # requires Python 3.6 or newer
+$ ./bootstrap.sh
 ```
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,15 @@
-python3 -c "import sys; assert sys.version_info.minor >= 6" && python3 symlinks.py
+DOTFILES_DIR=`pwd`
+
+# create directories
+find . -type d | grep -v "^./.git" | cut -d/ -f2- | xargs -I@ mkdir -p $HOME/@
+
+# create symlinks
+ln -sf $DOTFILES_DIR/.fdignore $HOME/.fdignore
+ln -sf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
+ln -sf $DOTFILES_DIR/.gitignore_global $HOME/.gitignore_global
+find .config -type f | cut -d/ -f2- | xargs -I@ ln -sf $DOTFILES_DIR/.config/@ $HOME/.config/@
+
+# setup neovim
 nvim --headless -c 'packadd packer.nvim' -c 'quitall'
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
