af3629303c8908e8ebe8d458b5710cde6bf0e7ce
Eduardo Cuducos <cuducos@users.noreply.github.com>
2022-01-24 11:23:51 -0500

    Configures Python virtualenv for Neovim

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
+NEOVIM_PYTHON_VENV=$HOME/.virtualenvs/neovim
 DOTFILES_DIR=`pwd`
 
 # create directories
@@ -8,6 +9,12 @@ ln -sf $DOTFILES_DIR/.fdignore $HOME/.fdignore
 ln -sf $DOTFILES_DIR/.gitconfig $HOME/.gitconfig
 ln -sf $DOTFILES_DIR/.gitignore_global $HOME/.gitignore_global
 find .config -type f | cut -d/ -f2- | xargs -I@ ln -sf $DOTFILES_DIR/.config/@ $HOME/.config/@
+
+# setup neovim's python virttualenv
+
+[ ! -d $NEOVIM_PYTHON_VENV ] && python3 -m venv $NEOVIM_PYTHON_VENV
+$NEOVIM_PYTHON_VENV/bin/pip install -U pip
+$NEOVIM_PYTHON_VENV/bin/pip install black neovim
 
 # setup neovim
 nvim --headless -c 'packadd packer.nvim' -c 'quitall'
