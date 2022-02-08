915b5da03a8cbe5f4c51d67a247ac2c66d69c449
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-02-08 17:13:43 -0500

    Adds hacked PackerReinstall in Neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,3 +13,27 @@ ReloadModule = function(name)
 	require("plenary.reload").reload_module(name)
 	vim.cmd("PackerCompile")
 end
+
+-- hacked PackerRemove cmd
+PackerReinstall = function(name) -- usage example => :lua PackerReinstall "yaml.nvim"
+	if package.loaded["packer"] == nil then
+		print("Packer not installed or not loaded")
+	end
+
+	local utils = require("packer.plugin_utils")
+	local suffix = "/" .. name
+
+	local opt, start = utils.list_installed_plugins()
+	for _, group in pairs({ opt, start }) do
+		if group ~= nil then
+			for dir, _ in pairs(group) do
+				if dir:sub(-string.len(suffix)) == suffix then
+					print("Removing", dir)
+                    vim.cmd("!rm -rf " .. dir)
+                    vim.cmd(":PackerSync")
+					return
+				end
+			end
+		end
+	end
+end
