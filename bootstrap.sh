b9084b680205b28ee4e10a6f31e763d3aa5d6317
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-02-15 11:35:37 -0500

    Installs Git Delta on bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 NEOVIM_PYTHON_VENV=$HOME/.virtualenvs/neovim
 DOTFILES_DIR=`pwd`
+ARCH=`uname -p`
+OS=`uname -o`
 
 # create directories
 find . -type d | grep -v "^./.git" | cut -d/ -f2- | xargs -I@ mkdir -p $HOME/@
@@ -14,6 +16,13 @@ find .config -type f | cut -d/ -f2- | xargs -I@ ln -sf $DOTFILES_DIR/.config/@ $
 [ ! -d $NEOVIM_PYTHON_VENV ] && python3 -m venv $NEOVIM_PYTHON_VENV
 $NEOVIM_PYTHON_VENV/bin/pip install -U pip
 $NEOVIM_PYTHON_VENV/bin/pip install black neovim
+
+# install git-delta (Debian-based x86_64)
+if [ "$ARCH" == "x86_64" &&  "$OS" == "GNU/Linux"]; then
+    wget https://github.com/dandavison/delta/releases/download/0.12.0/git-delta_0.12.0_amd64.deb
+    dpkg -i git-delta_0.12.0_amd64.deb
+    rm git-delta_0.12.0_amd64.deb
+fi
 
 # setup neovim
 nvim --headless -c 'packadd packer.nvim' -c 'quitall'
