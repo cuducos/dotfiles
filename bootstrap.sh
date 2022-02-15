3499f4aeed70cfc1eceb6b190b234bcdf2c8e476
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-02-15 13:27:43 -0500

    Stops using Git Delta

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,5 @@
 NEOVIM_PYTHON_VENV=$HOME/.virtualenvs/neovim
 DOTFILES_DIR=`pwd`
-ARCH=`uname -p`
-OS=`uname`
 
 # create directories
 find . -type d | grep -v "^./.git" | cut -d/ -f2- | xargs -I@ mkdir -p $HOME/@
@@ -17,15 +15,6 @@ if [ ! -d $NEOVIM_PYTHON_VENV ]; then
     python3 -m venv $NEOVIM_PYTHON_VENV
     $NEOVIM_PYTHON_VENV/bin/pip install -U pip
     $NEOVIM_PYTHON_VENV/bin/pip install black neovim
-fi
-
-# install git-delta (Debian-based x86_64)
-if [ "$ARCH" == "x86_64" ]; then
-    if [ "$OS" == "Linux"]; then
-        wget https://github.com/dandavison/delta/releases/download/0.12.0/git-delta_0.12.0_amd64.deb
-        dpkg -i git-delta_0.12.0_amd64.deb
-        rm git-delta_0.12.0_amd64.deb
-    fi
 fi
 
 # setup neovim
