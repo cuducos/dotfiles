cdbdd2f98689917f90d2bee7e591fe4a4a3c4b20
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-02-24 13:46:49 -0500

    Creates Git hunks mappings in Neovim

--- .config/nvim/lua/config/gitsigns.lua
+++ .config/nvim/lua/config/gitsigns.lua
@@ -1,1 +1,32 @@
-require("gitsigns").setup({ numhl = true })
+require("gitsigns").setup({
+	numhl = true,
+	on_attach = function(bufnr)
+		vim.api.nvim_buf_set_keymap(
+			bufnr,
+			"n",
+			"]h",
+			"&diff ? ']h' : '<cmd>Gitsigns next_hunk<CR>'",
+			{ noremap = true, expr = true }
+		)
+		vim.api.nvim_buf_set_keymap(
+			bufnr,
+			"n",
+			"[h",
+			"&diff ? '[h' : '<cmd>Gitsigns prev_hunk<CR>'",
+			{ noremap = true, expr = true }
+		)
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>hs", ":Gitsigns stage_hunk<CR>", { noremap = true })
+		vim.api.nvim_buf_set_keymap(bufnr, "v", "<leader>hs", ":Gitsigns stage_hunk<CR>", { noremap = true })
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>hr", ":Gitsigns reset_hunk<CR>", { noremap = true })
+		vim.api.nvim_buf_set_keymap(bufnr, "v", "<leader>hr", ":Gitsigns reset_hunk<CR>", { noremap = true })
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>hu", "<cmd>Gitsigns undo_stage_hunk<CR>", { noremap = true })
+		vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>hp", "<cmd>Gitsigns preview_hunk<CR>", { noremap = true })
+		vim.api.nvim_buf_set_keymap(
+			bufnr,
+			"n",
+			"<leader>hb",
+			"<cmd>Gitsigns toggle_current_line_blame<CR>",
+			{ noremap = true }
+		)
+	end,
+})
