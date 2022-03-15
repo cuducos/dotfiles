2b15df8c5882e5124a2a4810d0d6a0794b5a89a4
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-03-15 11:45:29 -0400

    Adds option to accept Copilot suggestion

--- .config/nvim/lua/config/copilot.lua
+++ .config/nvim/lua/config/copilot.lua
@@ -1,2 +1,4 @@
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
+
+vim.api.nvim_set_keymap("i", "<C-c>", 'copilot#Accept("<CR>")', { silent = true, expr = true, noremap = true })
