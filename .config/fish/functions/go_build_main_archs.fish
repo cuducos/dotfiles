777626d55a06a2380d166368429a3e81b97050da
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-05-24 08:47:08 -0400

    Adds go_build_main_archs.fish

---
+++ .config/fish/functions/go_build_main_archs.fish
@@ -0,0 +1,30 @@
+function go_build_main_archs
+    for dist in (go tool dist list)
+        set -l pwd (pwd)
+        set -l name (string split '/' $pwd -r -m1 -f2)
+        set -l os (string split '/' $dist -f1)
+        set -l arch (string split '/' $dist -f2)
+
+        switch $os
+        case linux darwin windows
+            switch $arch
+            case amd64 arm arm64
+                set -l output (string join "_" $name $os $arch)
+                switch $os
+                case windows
+                    set -l output "$output.exe"
+                end
+
+                set -l GOOS_BKP $GOOS
+                set -l GOARCH_BKP $GOARCH
+                set -x GOOS $os
+                set -x GOARCH $arch
+
+                go build -o $output main.go
+
+                set -x GOOS $GOOS_BKP
+                set -x GOARCH $GOARCH_BKP
+            end
+        end
+    end
+end
