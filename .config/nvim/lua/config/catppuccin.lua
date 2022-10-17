d96b5070870913d25e17c456fa88bc0d0eaaea24
Eduardo Vicente Gonçalves <eduardo.vicentegoncalves@shopify.com>
2022-10-17 13:59:14 -0400

    Properly setups Catpuccin integrations

--- .config/nvim/lua/config/catppuccin.lua
+++ .config/nvim/lua/config/catppuccin.lua
@@ -1,3 +1,25 @@
-require("catppuccin").setup({ indent_blankline = { colored_indent_levels = true } })
 vim.g.catppuccin_flavour = "latte"
+local colors = require("catppuccin.palettes").get_palette()
+require("catppuccin").setup({
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		lsp_trouble = true,
+		mason = true,
+		neogit = true,
+		notify = true,
+		nvimtree = true,
+		pounce = true,
+		telescope = true,
+		treesitter = true,
+		treesitter_context = true,
+		indent_blankline = {
+			enabled = true,
+			colored_indent_levels = true,
+		},
+	},
+	custom_highlights = {
+		NotifyINFOTitle = { fg = colors.sky, style = {} },
+	},
+})
 vim.cmd("colorscheme catppuccin")
