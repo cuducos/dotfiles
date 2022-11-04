ed02d80dfd653202d829f1dc8dec87f0ae6341ce
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-11-04 16:28:25 -0400

    Fixes function to get Go sub-package name

--- .config/nvim/lua/config/luasnip.lua
+++ .config/nvim/lua/config/luasnip.lua
@@ -8,12 +8,14 @@ luasnip.setup({
 })
 
 local go_pkg_name = function()
-	local cwd = vim.fn.getcwd()
-	local pkg = path:new(cwd)
-	if cwd ~= require("lspconfig.util").root_pattern(".git") then
+	local cur = vim.fn.expand("%")
+	local dir = path:new(cur):parent()
+	if dir == require("lspconfig.util").root_pattern(".git") then
 		return "main"
 	end
-	return pkg.filename
+
+	local last_sep = string.find(string.reverse(dir.filename), path.path.sep) - 1
+	return string.sub(dir.filename, -last_sep)
 end
 
 luasnip.add_snippets("go", {
@@ -33,7 +35,7 @@ luasnip.add_snippets("go", {
 		luasnip.insert_node(2),
 		luasnip.text_node({ ': %w", err)', "}" }),
 	}),
-	luasnip.snippet("pkg", { luasnip.text_node("package " .. go_pkg_name()) }),
+	luasnip.snippet("pkg", { luasnip.text_node({ "package " .. go_pkg_name(), "", "" }) }),
 })
 
 vim.api.nvim_create_autocmd("BufNewFile", {
