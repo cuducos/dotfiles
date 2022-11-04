cb2e480b705319d6d7b277ac2cfdc6e897f51254
Eduardo Vicente Gonçalves <eduardo.vicentegoncalves@shopify.com>
2022-11-04 15:42:51 -0400

    Auto add package line for new Go files in Neovim

--- .config/nvim/lua/config/luasnip.lua
+++ .config/nvim/lua/config/luasnip.lua
@@ -1,6 +1,15 @@
 local luasnip = require("luasnip")
+local path = require("plenary.path")
 
-luasnip.setup({})
+local go_pkg_name = function()
+	local cwd = vim.fn.getcwd()
+	local pkg = path:new(cwd)
+	if cwd ~= require("lspconfig.util").root_pattern(".git") then
+		return "main"
+	end
+	return pkg.filename
+end
+
 luasnip.add_snippets("go", {
 	luasnip.snippet("ifer", {
 		luasnip.text_node({ "if err != nil {", "\treturn " }),
@@ -18,4 +27,18 @@ luasnip.add_snippets("go", {
 		luasnip.insert_node(2),
 		luasnip.text_node({ ': %w", err)', "}" }),
 	}),
+	luasnip.snippet("pkg", { luasnip.text_node("package " .. go_pkg_name()) }),
+})
+
+vim.api.nvim_create_autocmd("BufNewFile", {
+	group = vim.api.nvim_create_augroup("GoAutoAddPkgLine", {}),
+	pattern = "*.go",
+	callback = function()
+		for _, snippet in pairs(luasnip.get_snippets("go")) do
+			if snippet.name == "pkg" then
+				luasnip.snip_expand(snippet, {})
+				return
+			end
+		end
+	end,
 })
