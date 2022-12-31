006f5ad03c68983196480e0cb84cc48229dd6528
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-12-31 17:25:12 -0500

    Rewrites wilder.nvim config in Lua

--- .config/nvim/lua/config/wilder.lua
+++ .config/nvim/lua/config/wilder.lua
@@ -1,48 +1,59 @@
-vim.cmd([[
-call wilder#setup({'modes': [':', '/', '?']})
-call wilder#set_option('renderer', wilder#popupmenu_renderer({
-      \ 'highlighter': wilder#basic_highlighter(),
-      \ }))
- call wilder#set_option('renderer', wilder#popupmenu_renderer(wilder#popupmenu_border_theme({
-      \ 'highlighter': wilder#basic_highlighter(),
-      \ 'highlights': {
-      \   'border': 'Normal',
-      \   'accent': wilder#make_hl('WilderAccent', 'Pmenu', [{}, {}, {'foreground': '#ab47bc'}]),
-      \ },
-      \ 'border': 'rounded',
-      \ 'left': [
-      \   ' ', wilder#popupmenu_devicons(),
-      \ ],
-      \ })))
-]])
+local wilder = require("wilder")
 
--- if fd is not installed, use different file_command and dir_command opts
-if vim.fn.executable("fd") == 1 then
-	vim.cmd([[
-call wilder#set_option('pipeline', [
-      \   wilder#branch(
-      \     wilder#python_file_finder_pipeline({
-      \       'file_command': ['fd', '-tf'],
-      \       'dir_command': ['fd', '-td'],
-      \       'filters': ['fuzzy_filter', 'difflib_sorter'],
-      \     }),
-      \     wilder#cmdline_pipeline(),
-      \     wilder#python_search_pipeline(),
-      \   ),
-      \ ])
-    ]])
-else
-	vim.cmd([[
-call wilder#set_option('pipeline', [
-      \   wilder#branch(
-      \     wilder#python_file_finder_pipeline({
-      \       'file_command': ['find', '.', '-type', 'f', '-printf', '%P\n'],
-      \       'dir_command': ['find', '.', '-type', 'd', '-printf', '%P\n'],
-      \       'filters': ['fuzzy_filter', 'difflib_sorter'],
-      \     }),
-      \     wilder#cmdline_pipeline(),
-      \     wilder#python_search_pipeline(),
-      \   ),
-      \ ])
-    ]])
+local find_cmd_for = function(type)
+	if vim.fn.executable("fd") == 1 then
+		return { "fd", "--type", type, "--hidden", "--follow", "--exclude", ".git" }
+	else
+		return { "find", ".", "-type", type, "-printf", "%P\n" }
+	end
 end
+
+local highlights_for = function(mode)
+	local fmt = { foreground = "#ab47bc", bold = true }
+	if mode == "Popup" then
+		fmt.background = "#eff1f4"
+	end
+	if mode == "Mini" then
+		fmt.background = "#d0d3dc"
+	end
+	result = {
+		border = "Normal",
+		accent = wilder.make_hl("WilderAccent" .. mode, "Pmenu", { { a = 1 }, { a = 1 }, fmt }),
+	}
+	return result
+end
+
+wilder.setup({ modes = { ":", "/" } })
+
+wilder.set_option("pipeline", {
+	wilder.branch(
+		wilder.python_file_finder_pipeline({
+			file_command = find_cmd_for("f"),
+			dir_command = find_cmd_for("d"),
+			filters = { "fuzzy_filter", "difflib_sorter" },
+		}),
+		wilder.cmdline_pipeline({ fuzzy = 2 }),
+		wilder.python_search_pipeline({ pattern = "fuzzy" })
+	),
+})
+
+local popup = wilder.popupmenu_renderer(wilder.popupmenu_palette_theme({
+	border = "rounded",
+	highlighter = wilder.basic_highlighter(),
+	highlights = highlights_for("Popup"),
+}))
+
+local mini = wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+	highlighter = wilder.basic_highlighter(),
+	highlights = highlights_for("Mini"),
+	border = "rounded",
+}))
+
+wilder.set_option(
+	"renderer",
+	wilder.renderer_mux({
+		[":"] = popup,
+		["/"] = mini,
+		substitute = mini,
+	})
+)
