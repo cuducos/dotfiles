7d959ce256480e1438b487d15f70c34bccfd140d
Eduardo Cuducos <cuducos@users.noreply.github.com>
2023-01-27 09:21:51 -0500

    Adds sextou Fish alias 🍻

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -112,3 +112,9 @@ end
 
 # brew's ruby
 set PATH /usr/local/opt/ruby/bin $PATH
+
+if type -q ezz
+  if type -q boteco
+    alias sextou "boteco (ezz --name Boteco --on today --at 17:00 -d 420 -p 42 -t America/Sao_Paulo)"
+  end
+end
