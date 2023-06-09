d137aa2587210b79b4334b54b51b30c5ad709c8f
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2023-06-09 02:36:37 -0400

    Limits wed notifications to 1 per hour

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -121,8 +121,21 @@ if type -q ezz
   end
 end
 
+# wed
 if type -q wed
     if [ (uname -o | string lower) = 'darwin' ]
-        wed notify
+        set -l WED_NOTIFICATION_FILE $HOME/.last_wed_notification
+        set -l NOW (date +%s)
+        set -l ONE_HOUR 3600
+
+        if [ ! -e $WED_NOTIFICATION_FILE ]
+            echo "0" > $WED_NOTIFICATION_FILE
+        end
+
+        set -l LAST_WED_NOTIFICATION (cat $WED_NOTIFICATION_FILE)
+        if [ $NOW -gt (math $LAST_WED_NOTIFICATION + $ONE_HOUR) ]
+            wed notify
+            echo $NOW > $WED_NOTIFICATION_FILE
+        end
     end
 end
