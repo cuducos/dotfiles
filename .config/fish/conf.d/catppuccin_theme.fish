13250ee9b14d4455cb68c5f3288d0934075d6237
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2024-01-02 14:12:15 -0500

    Fixes theme selector (month with leading zero)

--- .config/fish/conf.d/catppuccin_theme.fish
+++ .config/fish/conf.d/catppuccin_theme.fish
@@ -1,14 +1,14 @@
 switch (date +%m)
-    case 1 2 12
+    case 01 2 12
         set -g sunrise 7
         set -g sunset 16
-    case 3 4 5
+    case 03 04 05
         set -g sunrise 7
         set -g sunset 16
-    case 6 7 8
+    case 06 07 08
         set -g sunrise 5
         set -g sunset 20
-    case 9 10 11
+    case 09 10 11
         set -g sunrise 7
         set -g sunset 17
 end
