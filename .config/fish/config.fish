66a8d57480c949f3bf8340d1e6fe3e8daf1e0f24
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2024-04-15 18:16:00 -0400

    Brings `wed` back to config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -105,8 +105,27 @@ set --export BAT_THEME OneHalfLight
 # direnv
 
 if type -q direnv
-  direnv hook fish | source
+    direnv hook fish | source
 end
 
 # brew's ruby
 set PATH /usr/local/opt/ruby/bin $PATH
+
+# wed
+if type -q wed
+    if [ (uname -o | string lower) = darwin ]
+        set -l WED_NOTIFICATION_FILE $HOME/.last_wed_notification
+        set -l NOW (date +%s)
+        set -l ONE_HOUR 3600
+
+        if [ ! -e $WED_NOTIFICATION_FILE ]
+            echo 0 >$WED_NOTIFICATION_FILE
+        end
+
+        set -l LAST_WED_NOTIFICATION (cat $WED_NOTIFICATION_FILE)
+        if [ $NOW -gt (math $LAST_WED_NOTIFICATION + $ONE_HOUR) ]
+            wed notify
+            echo $NOW >$WED_NOTIFICATION_FILE
+        end
+    end
+end
