b20819fe8fde93dab64753737bcb1d203a2a486c
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2024-05-17 12:46:36 -0400

    Removes CloseBufferIfItHasNoPendingLspDiagnostic

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,2 @@
 require("editor")
 require("plugins")
-
-CloseBufferIfItHasNoPendingLspDiagnostic = function()
-	local buf = vim.api.nvim_get_current_buf()
-	local diagnostics = vim.diagnostic.get(buf)
-
-	for _, diagnostic in ipairs(diagnostics) do
-		if diagnostic.severity ~= vim.lsp.protocol.DiagnosticSeverity.Hint then
-			vim.api.nvim_buf_delete(buf, { force = true, unload = true })
-			return
-		end
-	end
-end
-
-vim.cmd("command! -nargs=0 CloseBufferIfItHasNoPendingLspDiagnostic lua CloseBufferIfItHasNoPendingLspDiagnostic()")
