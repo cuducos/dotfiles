8f536a28c7f9f2059f760f572fd195b61863d522
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2024-05-17 10:55:16 -0400

    Addapts some keymaps to Neovim 0.10 native ones

--- .config/nvim/lua/config/gitsigns.lua
+++ .config/nvim/lua/config/gitsigns.lua
@@ -14,8 +14,8 @@ require("gitsigns").setup({
 		virt_text_pos = "right_align",
 	},
 	on_attach = function(_)
-		vim.keymap.set("n", "+", require("gitsigns").next_hunk, opts)
-		vim.keymap.set("n", "_", require("gitsigns").prev_hunk, opts)
+		vim.keymap.set("n", "[g", require("gitsigns").next_hunk, opts)
+		vim.keymap.set("n", "]g", require("gitsigns").prev_hunk, opts)
 		vim.keymap.set("n", "<Leader>hp", require("gitsigns").preview_hunk, opts)
 		vim.keymap.set("n", "<Leader>hs", require("gitsigns").stage_hunk, opts)
 		vim.keymap.set("n", "<Leader>hr", require("gitsigns").reset_hunk, opts)
--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -17,8 +17,6 @@ M.on_attach = function(client, _)
 		{ "n", "gD", vim.lsp.buf.type_definition, opts },
 		{ "n", "gr", vim.lsp.buf.rename, opts },
 		{ "n", "gs", vim.lsp.buf.hover, opts },
-		{ "n", "=", vim.diagnostic.goto_next, opts },
-		{ "n", "-", vim.diagnostic.goto_prev, opts },
 		{ "n", "<Leader>s", require("telescope.builtin").lsp_document_symbols, opts },
 		{ "n", "<Leader>S", require("telescope.builtin").lsp_workspace_symbols, opts },
 		{ "n", "<Leader>r", require("telescope.builtin").lsp_references, opts },
