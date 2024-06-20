45361a86d492474e80c2d58727036d822c8b02c3
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2024-06-20 12:21:29 -0400

    Adds Trouble to Neovim's hardtime

--- .config/nvim/lua/config/hardtime.lua
+++ .config/nvim/lua/config/hardtime.lua
@@ -2,5 +2,5 @@ require("hardtime").setup({
 	disable_mouse = false,
 	notification = false,
 	disabled_keys = {},
-	disabled_filetypes = { "NeogitStatus", "NvimTree", "lazy", "mason" },
+	disabled_filetypes = { "NeogitStatus", "NvimTree", "lazy", "mason", "trouble" },
 })
