a1028510132cdc056afc3ce8480464bc0943c6dd
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2024-06-28 08:47:43 -0400

    Adds keymap to insert/remove `pytest`'s set trace call

--- .config/nvim/lua/editor.lua
+++ .config/nvim/lua/editor.lua
@@ -198,3 +198,29 @@ vim.keymap.set("n", "<cr>", function()
 	handler:close()
 	return "gF"
 end, {})
+
+vim.keymap.set("n", "<Leader>st", function()
+	if vim.bo.filetype ~= "python" then
+		return
+	end
+
+	local cmd = "import pytest; pytest.set_trace()"
+	local variants = { "import pytest", "pytest.set_trace()" }
+
+	local cleaned = vim.api.nvim_get_current_line():match("^%s*(.-)%s*$")
+	if cleaned == variants[1] or cleaned == variants[2] or cleaned == cmd then
+		local bufnr = vim.api.nvim_get_current_buf()
+		local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+		for i = #lines, 1, -1 do
+			local line = lines[i]:match("^%s*(.-)%s*$")
+			if line == variants[1] or line == variants[2] or line == cmd then
+				vim.api.nvim_buf_set_lines(bufnr, i - 1, i, false, {})
+			end
+		end
+	else
+		local _, col = unpack(vim.api.nvim_win_get_cursor(0))
+		local current_line = vim.api.nvim_get_current_line()
+		local new_line = current_line:sub(1, col) .. " " .. cmd .. current_line:sub(col + 1)
+		vim.api.nvim_set_current_line(new_line)
+	end
+end)
