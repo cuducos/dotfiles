cccf0dcffa2589de887d9f910e80ba3c28a3a4ba
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2024-08-08 20:24:20 -0400

    Adds `todo-comments.nvim`

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -218,6 +218,12 @@ local plugins = {
 			"nvim-tree/nvim-web-devicons",
 		},
 	},
+	{
+		"folke/todo-comments.nvim",
+		config = function()
+			require("config.todo")
+		end,
+	},
 
 	-- navigation & selection
 	{
