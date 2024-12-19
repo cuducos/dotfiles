88a8bfa69288156cfb6eb715498806d8caf84db9
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2024-12-19 16:17:20 -0500

    Adds keymaps do CodeCompanion

--- .config/nvim/lua/config/codecompanion.lua
+++ .config/nvim/lua/config/codecompanion.lua
@@ -1,8 +1,10 @@
 require("codecompanion").setup({
+	display = { diff = { enabled = false } },
 	strategies = {
-		chat = {
-			adapter = "openai",
-		},
-		inline = nil,
+		chat = { adapter = "openai" },
+		inline = { adapter = "openai" },
 	},
 })
+vim.keymap.set({ "n", "v" }, "<Leader>ci", "<cmd>CodeCompanion<cr>")
+vim.keymap.set({ "n", "v" }, "<Leader>cc", "<cmd>CodeCompanionChat<cr>")
+vim.cmd([[cab cc CodeCompanion]]) -- Expand 'cc' into 'CodeCompanion' in the command line
