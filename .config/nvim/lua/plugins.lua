9420f7b1e03f2abfde288d34cd393981c0f6ce9d
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2024-12-18 19:05:00 -0500

    Configures snippets auto-completion

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -84,6 +84,20 @@ local plugins = {
 				use_nvim_cmp_as_default = true,
 				nerd_font_variant = "mono",
 			},
+			snippets = {
+				expand = function(snippet)
+					require("luasnip").lsp_expand(snippet)
+				end,
+				active = function(filter)
+					if filter and filter.direction then
+						return require("luasnip").jumpable(filter.direction)
+					end
+					return require("luasnip").in_snippet()
+				end,
+				jump = function(direction)
+					require("luasnip").jump(direction)
+				end,
+			},
 			sources = {
 				providers = {
 					copilot = {
@@ -93,7 +107,7 @@ local plugins = {
 						async = true,
 					},
 				},
-				default = { "copilot", "lsp", "buffer", "path", "snippets" },
+				default = { "copilot", "lsp", "buffer", "path", "luasnip" },
 			},
 		},
 		opts_extend = { "sources.default" },
