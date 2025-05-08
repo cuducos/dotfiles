aa598830b4c36cfef59311a2d06c02c7fdb0c5a8
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2025-05-08 16:57:33 -0400

    Removes deprecated `vim.lsp.handlers.signature_help`

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -12,91 +12,83 @@ local has_float = function()
 	return false
 end
 
-M.on_attach = function(client, _)
-	local opts = { silent = true, noremap = true, buffer = true }
-	local mappings = {
-		{ "n", "gd", vim.lsp.buf.definition, opts },
-		{ "n", "gD", vim.lsp.buf.type_definition, opts },
-		{ "n", "gr", vim.lsp.buf.rename, opts },
-		{
-			"n",
-			"<Leader>a",
-			function()
-				if vim.bo.filetype == "python" then
-					vim.lsp.buf.code_action({ apply = true, context = { only = { "source.fixAll.ruff" } } })
-				end
-				vim.lsp.buf.format({ async = true })
-			end,
-		},
-		{ "n", "<Leader><Leader>", vim.lsp.buf.code_action },
-		{
-			"n",
-			"<Leader>ih",
-			function()
-				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled(), { bufnr = 0 })
-			end,
-		},
-	}
-	for _, mapping in pairs(mappings) do
-		vim.keymap.set(unpack(mapping))
-	end
+local mappings = {
 
-	local diagnostic = vim.api.nvim_create_augroup("DiagnosticFloat", { clear = true })
-	vim.api.nvim_create_autocmd("CursorHold", {
-		callback = function()
-			if has_float() then
-				return
-			end
-
-			vim.diagnostic.open_float(nil, { focus = false })
+	{ "n", "gd", vim.lsp.buf.definition, opts },
+	{ "n", "gD", vim.lsp.buf.type_definition, opts },
+	{ "n", "gr", vim.lsp.buf.rename, opts },
+	{
+		"n",
+		"K",
+		function()
+			vim.lsp.buf.signature_help({ border = "rounded" })
 		end,
-		group = diagnostic,
-	})
-
-	local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-	for type, icon in pairs(signs) do
-		local hl = "DiagnosticSign" .. type
-		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-	end
-
-	if client.server_capabilities.document_highlight then
-		vim.api.nvim_create_augroup("LspDocumentHighlight", {})
-		vim.api.nvim_create_autocmd("CursorHold", {
-			pattern = "<buffer>",
-			callback = function()
-				vim.lsp.buf.document_highlight()
-			end,
-		})
-		vim.api.nvim_create_autocmd("CursorMoved", {
-			pattern = "<buffer>",
-			callback = function()
-				vim.lsp.buf.clear_references()
-			end,
-		})
-	end
-	vim.lsp.inlay_hint.enable()
-end
+		opts,
+	},
+	{
+		"n",
+		"<Leader>a",
+		function()
+			if vim.bo.filetype == "python" then
+				vim.lsp.buf.code_action({ apply = true, context = { only = { "source.fixAll.ruff" } } })
+			end
+			vim.lsp.buf.format({ async = true })
+		end,
+	},
+	{ "n", "<Leader><Leader>", vim.lsp.buf.code_action },
+	{
+		"n",
+		"<Leader>ih",
+		function()
+			vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled(), { bufnr = 0 })
+		end,
+	},
+}
 
 M.make_config = function()
-	local border = {
-		{ "╭", "FloatBorder" },
-		{ "─", "FloatBorder" },
-		{ "╮", "FloatBorder" },
-		{ "│", "FloatBorder" },
-		{ "╯", "FloatBorder" },
-		{ "─", "FloatBorder" },
-		{ "╰", "FloatBorder" },
-		{ "│", "FloatBorder" },
-	}
-	local handlers = {
-		["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
-		["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
-	}
-	local capabilities = vim.lsp.protocol.make_client_capabilities()
 	return {
-		handlers = handlers,
-		on_attach = M.on_attach,
-		capabilities = blink.get_lsp_capabilities(capabilities),
+		capabilities = blink.get_lsp_capabilities(vim.lsp.protocol.make_client_capabilities()),
+		on_attach = function(client, _)
+			local opts = { silent = true, noremap = true, buffer = true }
+			for _, mapping in pairs(mappings) do
+				vim.keymap.set(unpack(mapping))
+			end
+
+			local diagnostic = vim.api.nvim_create_augroup("DiagnosticFloat", { clear = true })
+			vim.api.nvim_create_autocmd("CursorHold", {
+				callback = function()
+					if has_float() then
+						return
+					end
+
+					vim.diagnostic.open_float(nil, { focus = false })
+				end,
+				group = diagnostic,
+			})
+
+			local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			end
+
+			if client.server_capabilities.document_highlight then
+				vim.api.nvim_create_augroup("LspDocumentHighlight", {})
+				vim.api.nvim_create_autocmd("CursorHold", {
+					pattern = "<buffer>",
+					callback = function()
+						vim.lsp.buf.document_highlight()
+					end,
+				})
+				vim.api.nvim_create_autocmd("CursorMoved", {
+					pattern = "<buffer>",
+					callback = function()
+						vim.lsp.buf.clear_references()
+					end,
+				})
+			end
+			vim.lsp.inlay_hint.enable()
+		end,
 	}
 end
 
