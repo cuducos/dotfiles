d5ac44e9d16624f5cc091bbd3053d94e6b1c8857
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2025-05-13 08:50:07 -0400

    Fixes recursive call on Code Companion config

--- .config/nvim/lua/config/codecompanion.lua
+++ .config/nvim/lua/config/codecompanion.lua
@@ -1,9 +1,26 @@
+local default_prompt = require("codecompanion.config").config.opts.system_prompt({})
+local extras = {
+	"When replying with code, the code must:",
+	"- Be modern production-ready, clean and expressive code",
+	"- Use code optimized for computational resources (memory and CPU)",
+	"- Avoid useless comments, the code should be expressive by itself",
+	"- Use meanignless variable/function/method names",
+	"- If in Python, avoid generic imports as `import foo` (use `from foo import bar` instead)",
+	"- If in Go, keep scope variable names short",
+}
+
 require("codecompanion").setup({
 	display = { diff = { enabled = false } },
 	strategies = {
 		chat = { adapter = "deepseek" },
 		inline = { adapter = "deepseek" },
 	},
+	opts = {
+		system_prompt = function()
+			return default_prompt .. "\n\n" .. table.concat(extras, "\n")
+		end,
+	},
 })
+
 vim.keymap.set({ "n", "v" }, "<Leader>ci", "<cmd>CodeCompanion<cr>")
 vim.keymap.set({ "n", "v" }, "<Leader>cc", "<cmd>CodeCompanionChat Toggle<cr>")
