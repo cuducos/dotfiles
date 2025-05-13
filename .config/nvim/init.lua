01864d6c7f746c7dcd05ab70ee186f1da620e760
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2025-05-13 08:47:57 -0400

    Removes `MasonCleanupLockfiles`

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,2 @@
 require("editor")
 require("plugins")
-
-MasonCleanupLockfiles = function()
-	local dir = require("mason-core.path").package_build_prefix()
-	for path in io.popen("fd -e lock . " .. dir):lines() do
-		local message = "Delete " .. path .. "? [y/n] "
-		local response = vim.fn.input(message)
-		if response == "y" then
-			os.remove(path)
-		end
-	end
-end
-
-vim.cmd("command! -nargs=0 MasonCleanupLockfiles lua MasonCleanupLockfiles()")
