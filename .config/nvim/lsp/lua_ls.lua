cdd76bc206ebf6e0d792c97ddc81dda98e9efbab
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2025-09-14 21:00:34 -0400

    Fixes `vim` in Lua LS

--- .config/nvim/lsp/lua_ls.lua
+++ .config/nvim/lsp/lua_ls.lua
@@ -2,9 +2,11 @@ return {
 	settings = {
 		Lua = {
 			runtime = { version = "LuaJIT" },
-			diagnostics = { globals = { "vim" } },
 			completion = { callSnippet = "Replace" },
 			hint = { enable = true },
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
 		},
 	},
 }
